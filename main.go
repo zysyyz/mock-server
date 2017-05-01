239cb6fe03b7e6c5a12e3f589794f978b336277f
huzhifeng <huzhifeng@douyu.tv>
2017-05-01 15:53:52 +0800

    Initialized main.go

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/gin-gonic/gin.v1"
+	"io/ioutil"
+)
+
+func main() {
+	r := gin.Default()
+	r.NoRoute(func(c *gin.Context) {
+		str := fmt.Sprintf("%s", c.Request.URL.Query())
+		fmt.Println("Queries:", str)
+		body, err := ioutil.ReadAll(c.Request.Body)
+		if err == nil {
+			str2 := fmt.Sprintf("%s", body)
+			fmt.Println("Request Body:", str2)
+
+		}
+		c.JSON(200, gin.H{"status": "ok"})
+	})
+	r.Run()
+}
